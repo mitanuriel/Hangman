f64af26f3239ceaeec436199d6aea339a29bb7dd
Elina K. <145558996+mitanuriel@users.noreply.github.com>
2025-01-27 14:27:16 +0100

    Set basics of game logic

--- src/Hangman.py
+++ src/Hangman.py
@@ -4,3 +4,27 @@ words = [ "tree", "basket", "chair", "paper", "python"]
 word = choice(words)
 guessed,lives,game_over = [ ],7, False
 
+guesses = ["_"] * len(word)
+
+while not game_over:
+    ans = input("Type quit or guess a letter: ").lower()
+    if ans == "quit":
+        print("Thanks for playing.")
+        game_over = True
+
+while not game_over:
+    hidden_word = "".join(guesses)
+print("Word to guess: {}".format(hidden_word))
+print( "Lives: {}".format(lives))
+ans = input( )
+
+game_over = True
+elif ans in word:
+print("You guessed correctly!")
+else:
+lives -= 1
+print("Incorrect, you lost a life.")
+
+ans = input()
+clear_output()
+if ans == 'quit':
